971f56293fc6f150556b28688f9d79ee903160ce
Ian Lenehan <ian.lenehan@blake.com.au>
2018-10-23 13:12:19 +1100

    get ready for class

--- likes/app.js
+++ likes/app.js
@@ -1,3 +1,22 @@
+const posts = [
+  {
+    id: 1,
+    content: 'Bacon ipsum dolor amet short loin sed exercitation, ad consectetur filet mignon pastrami. Deserunt capicola doner dolore, do incididunt anim ball tip in hamburger sunt tongue jowl. Tongue aute andouille, ad shoulder sunt landjaeger. Bresaola shoulder magna frankfurter quis tenderloin dolore ut reprehenderit veniam in enim cupidatat. Nostrud sed corned beef exercitation dolore flank kevin jerky fatback culpa do strip steak.',
+    date: '2018-09-15',
+    user: 'Jane Doe',
+    likes: 0,
+    image: 'http://fillmurray.com/300/300'
+  },
+  {
+    id: 2,
+    content: 'Fugiat nisi eu, est incididunt buffalo dolore laborum ham fatback meatball salami tail rump. Capicola eiusmod salami beef ribs, shank alcatra jowl bacon cow. Meatball deserunt flank, swine ipsum kielbasa tail dolore occaecat rump incididunt fugiat magna non.',
+    date: '2018-09-17',
+    user: 'Me',
+    likes: 1,
+    image: 'http://placecage.com/300/300'
+  },
+]
+
 $(document).ready(function () {
   console.log('ready');
 })
